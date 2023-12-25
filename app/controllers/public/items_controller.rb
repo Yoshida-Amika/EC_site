c455f38ed0cfde297002e64c79a0babdee139b87
EC2 Default User <ec2-user@ip-172-31-15-78.ap-northeast-1.compute.internal>
2023-12-25 12:32:39 +0000

    Commit

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,17 +1,21 @@
 class Public::ItemsController < ApplicationController
 
   def index
-   @items = Item.all
+   @item = Item.all
    @customer = current_customer
   end
-  
+
   def show
     @customer = current_customer
   end
-  
-  
+
+
   private
-  
-  
+
+    def item_params
+      params.require(:item).permit(:image, :name, :introduction, :price)
+    end
+
+
 end
 
