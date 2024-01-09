75236839dae5f55c40908b35d460f06f53f0110f
EC2 Default User <ec2-user@ip-172-31-15-78.ap-northeast-1.compute.internal>
2024-01-09 13:10:22 +0000

    Commit

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -18,10 +18,19 @@ end
 
 
 def create
-  cart_item = CartItem.new(cart_item_params)
-  cart_item.customer_id = current_customer.id
-  cart_item.save
-  redirect_to  cart_items_path
+  @cart_item = current_customer.cart_items.new(cart_item_params)
+  @cart_item.customer_id = current_customer.id
+  if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+		 cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+		 cart_item.amount += params[:cart_item][:amount].to_i
+		 cart_item.save
+		 redirect_to cart_items_path
+	elsif @cart_item.save
+            @cart_items = current_customer.cart_items.all
+            render'index'
+  else
+            render'index'
+	end
 end
 
 def destroy
